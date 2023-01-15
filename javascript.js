cde4a9d96afe85abfdc67380f1b99b541612059e
Boshi <8356799+Boxis@users.noreply.github.com>
2023-01-14 22:32:23 -0500

    Add buttons, game logic, DOM elements

--- javascript.js
+++ javascript.js
@@ -5,69 +5,76 @@ function getComputerChoice() {
 }
 
 
-const rock = document.getElementById("rock");
-
-// rock.addEventListener("click", playRound(playerSelection="rock"));
-
 const buttons = document.querySelectorAll('button');
+
+var scoreHuman = 0;
+var scoreCPU = 0;
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        playRound(button.id);
-    })
-})
+        result = playRound(button.id);
+        const container = document.querySelector('#output-messages');
 
-
-function playRound(playerSelection) {
-    const computerSelection = getComputerChoice().toLowerCase();
-    console.log(playerSelection);
-    console.log(computerSelection);
-
-    if (playerSelection == computerSelection) {
-        result = "It's a draw!";
-    } else if ((playerSelection =="rock" && computerSelection=="scissors") 
-        || (playerSelection=="paper" && computerSelection=="rock")
-        || (playerSelection=="scissors" && computerSelection=="paper")) {
-        result = `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = result;
     
-    console.log(result);
-
-    return result;
-}
-
-
-function game() {
-    let scoreHuman = 0;
-    let scoreCPU = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please enter your selection: ").toLowerCase();
-        // const playerSelection = "rock";
-        const computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
+        container.appendChild(content);
 
         if (result.includes("Win")==true) {
             scoreHuman++;
         } else if (result.includes("Lose")==true) {
             scoreCPU++;
         }
+
+        finalOutput = checkScore(scoreHuman, scoreCPU);
+        console.log(scoreHuman, scoreCPU);
+
+        document.getElementById('human-score').innerHTML = "Human: " + scoreHuman;
+        document.getElementById('cpu-score').innerHTML = "CPU: " + scoreCPU;
+
+        if (scoreHuman==5|| scoreCPU==5) {
+            scoreHuman=0;
+            scoreCPU=0;
+            removeAllChildNodes(container);
+            content.textContent = finalOutput;
+            container.appendChild(content);
+        }
+    })
+})
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
+}
 
-    console.log(scoreHuman);
-    console.log(scoreCPU);
 
-    if (scoreHuman==scoreCPU) {
-        message = "It's a tie!";
-    } else if (scoreHuman > scoreCPU) {
-        message = "You win!";
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+
+    if (playerSelection == computerSelection) {
+        result = `It's a draw! Both chose ${playerSelection}`;
+    } else if ((playerSelection =="Rock" && computerSelection=="Scissors") 
+        || (playerSelection=="Paper" && computerSelection=="Rock")
+        || (playerSelection=="Scissors" && computerSelection=="Paper")) {
+        result = `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        message = "Computer wins!";
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 
+    return result;
+}
+
+function checkScore(scoreHuman, scoreCPU) {
+    if (scoreHuman == 5) {
+        message = 'You win!';
+    } else if (scoreCPU == 5) {
+        message = 'CPU wins!';
+    } else {
+        message = 'The game continues!';
+    }
     return message;
 }
 
